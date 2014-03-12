da14b562f7f050e0cdbcae92255d9caf55f621c3
Alexander Rössler <mail.aroessler@gmail.com>
2014-03-12 11:23:44 +0100

    decode to something useful

--- subscribertest.cpp
+++ subscribertest.cpp
@@ -23,16 +23,42 @@ SubscriberTest::SubscriberTest(QQuickItem *parent) :
 void SubscriberTest::pingReceived(const QList<QByteArray> &message)
 {
     QString newMessage;
+    QString topic;
+    QByteArray data;
     pb::Container container;
 
-    foreach (QByteArray bytes, message)
+    if (message.size() != 2)    // we should receive topic and encoded protobuf string
     {
-        newMessage.append(QString(bytes) + ", ");
-        qDebug() << bytes;
-        container.ParseFromString(QString(bytes).toStdString());
-        qDebug() << container.pin_size() << container.note_size();
+        return;
     }
 
+    topic = QString(message.at(0));
+    data = message.at(1);
+
+    container.ParseFromArray(data.data(), data.size());
+
+    int pinCount;
+    int noteCount;
+
+    pinCount = container.pin_size();
+    noteCount = container.note_size();
+
+    for (int i = 0; i < pinCount; ++i)
+    {
+        newMessage.append(QString("Pin: %1\n").arg(QString::fromStdString(container.pin(i).name())));
+        switch (container.pin(i).type())
+        {
+            case pb::HAL_BIT:   newMessage.append(QString("bit: %1\n").arg(container.pin(i).halbit())); break;
+            case pb::HAL_FLOAT: newMessage.append(QString("float: %1\n").arg(container.pin(i).halfloat())); break;
+            case pb::HAL_S32:   newMessage.append(QString("s32: %1\n").arg(container.pin(i).hals32())); break;
+            case pb::HAL_U32:   newMessage.append(QString("u32: %1\n").arg(container.pin(i).halu32())); break;
+        }
+    }
+
+    for (int i = 0; i < noteCount; ++i)
+    {
+        newMessage.append(QString("Note: %1\n").arg(QString::fromStdString(container.note(i))));
+    }
     qDebug() << newMessage;
 
     m_message = newMessage;
